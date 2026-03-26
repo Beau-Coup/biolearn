6f3971f813943626350f66da7233afeedb17dc53
Hanna <hanna.krasowski@web.de>
2026-03-25 18:02:47 -0700

    systems for CORA and reachablity working for l4dc system

---
+++ matlab/example_stl_Hill.m
@@ -0,0 +1,111 @@
+function res = example_stl_Hill
+% example_stl_BNN - example of signal temporal logic
+% checking of the room heating model
+%
+% Syntax:
+%    res = example_stl_BNN()
+%
+% Inputs:
+%    no
+%
+% Outputs:
+%    res - boolean
+
+% ------------------------------ BEGIN CODE -------------------------------
+%y_test = BNN([7.34164577e-05, 2.33403951e-04, 1.70125806e-04, 1.64744608e-04, 2.10874017e-06, 1.51597852e-06, 0.0, 0.1, 0.3]);
+
+% Parameters --------------------------------------------------------------
+
+params.tFinal = 20;                                      % final time
+
+options.zonotopeOrder = 50;       
+options.intermediateOrder = 50;
+options.errorOrder = 20;
+
+% reachability algorithm
+options.alg = 'poly';
+options.tensorOrder = 3;
+
+options.timeStep = 0.1;
+options.taylorTerms = 4;
+
+
+% System Dynamics ---------------------------------------------------------
+
+% system with uncertain parameters
+BioNN = nonlinearSys(@BioTransmission);
+
+%%%%%%%%%%% TESTING
+simOpt.points = 1;
+
+params.R0 = zonotope([0.25; 0.25; 0.25; 0.2; 0.95; 0.95], diag([0.15*ones(4,1);0.05*ones(2,1)])); 
+params.tFinal = 40;   
+
+% NOW call the solver
+%[t_out, y_out] = ode15s(@BNN, ts, y0, u0, options);
+% random simulation
+traj = simulateRandom(BioNN,params,simOpt);
+
+figure;
+hold on;
+
+for i = 1:length(traj)
+    t_vec = traj(i).t;
+    %plot(t_vec, traj(i).x(7, :), 'LineWidth', 1.5);
+    plot(t_vec, traj(i).x(6, :), 'LineWidth', 1.5,'DisplayName', 'State 6');
+    plot(t_vec, traj(i).x(5, :), 'LineWidth', 1.5,'DisplayName', 'State 5');
+    plot(t_vec, traj(i).x(4, :), 'LineWidth', 1.5,'DisplayName', 'State 4');
+    plot(t_vec, traj(i).x(3, :), 'LineWidth', 1.5,'DisplayName', 'State 3');
+    plot(t_vec, traj(i).x(2, :), 'LineWidth', 1.5,'DisplayName', 'State 2');
+    plot(t_vec, traj(i).x(1, :), 'LineWidth', 1.5,'DisplayName', 'State 1');
+end
+
+% Add the threshold line
+
+hold off;
+xlabel('Time (h)');
+ylabel('Species concentration');
+title('Hill model');
+grid on;
+
+
+% Reachability Analysis ---------------------------------------------------        
+
+% compute reachable set with uncertain parameters
+options.intermediateTerms = 4;
+timerVal = tic;
+RcontParam = reach(BioNN, params, options);
+tComp = toc(timerVal);
+disp(['computation time of reachable set with uncertain parameters: ',num2str(tComp)]);
+
+
+% Visualization -----------------------------------------------------------
+figure;
+dims = {[1,2],[3,4],[5,6]};
+
+% plot different projections
+for i = 1:length(dims)
+
+    figure; hold on; box on;
+    projDims = dims{i};
+    useCORAcolors("CORA:contDynamics", 2)
+
+    % plot reachable sets
+    plot(RcontParam,projDims,'DisplayName','parametric');
+
+    % plot initial set
+    plot(RcontParam.R0,projDims,'DisplayName','Initial set');
+
+    % plot simulation results
+    plot(traj,projDims,'DisplayName','Simulations');
+
+    % label plot
+    xlabel(['x_{',num2str(projDims(1)),'}']);
+    ylabel(['x_{',num2str(projDims(2)),'}']);
+    legend();
+end
+
+
+end
+
+% ------------------------------ END OF CODE ------------------------------
---
+++ matlab/quadrocopterControlledSimplified.m
@@ -0,0 +1,70 @@
+function dx = quadrocopterControlledSimplified(x,u)
+
+% ------------------------------ BEGIN CODE -------------------------------
+
+% x_1 = p_n
+% x_2 = p_e
+% x_3 = h
+
+% x_7 = u
+% x_8 = v
+% x_9 = w
+
+% x_4 = phi
+% x_5 = theta
+% x_6 = psi
+
+% x_10 = p
+% x_11 = q
+% x_12 = r
+
+% u_1 desired height
+
+% parameters
+g = 9.81; %[m/s^2], gravity constant
+R = 0.1;
+l = 0.5;
+M_rotor = 0.1;
+M = 1;
+P = 10;
+D = 3;
+m = M + 4*M_rotor;
+u_1 = 1; % u_1 desired height
+
+% auxiliary parameters
+J_x = 2*M*R^2/5 + 2*l^2*M_rotor;
+J_y = J_x;
+J_z = 2*M*R^2/5 + 4*l^2*M_rotor;
+
+% height control (PD)
+F_temp = m*g - P*(x(3) - u_1) - D*x(9);
+F = clip(F_temp, 0.5 * m * g, 1.5 * m * g);
+
+% desired 
+
+% roll control (PD)
+tau_phi = -x(4) - x(10);
+
+% pitch control (PD)
+tau_theta = -x(5) - x(11);
+
+% heading is uncontrolled:
+tau_psi = 0;
+
+dx(1,1) = x(7);
+dx(2,1) = x(8);
+dx(3,1) = x(9);
+
+dx(4,1) = x(10); %dot{phi}
+dx(5,1) = x(11); %dot{theta}
+dx(6,1) = x(12); %dot{psi}
+
+dx(7,1) = F/m *(-cos(x(4))*sin(x(5))*cos(x(6))-sin(x(4))*sin(x(6)));
+dx(8,1) = F/m * (-cos(x(4))*sin(x(5))*sin(x(6))+sin(x(4))*cos(x(6)));
+dx(9,1) = F/m * (cos(x(4))*cos(x(5))) - g;
+
+dx(10,1) = (1/J_x)*tau_phi;
+dx(11,1) = (1/J_y)*tau_theta;
+dx(12,1) = (1/J_z)*tau_psi;
+
+% ------------------------------ END OF CODE ------------------------------
